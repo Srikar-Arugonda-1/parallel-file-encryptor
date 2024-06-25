5eb0862701d22789798cce460e80b014555416dc
Srikar-Arugonda-1 <cs22btech11008@iith.ac.in>
2024-06-25 23:30:37 +0530

    Created IO and Task class

---
+++ src/app/fileHandling/ReadEnv.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "IO.hpp"
+
+class ReadEnv
+{
+    public:
+        std::string getenv() {
+            std::string env_path = ".env";
+            IO io(env_path);
+            std::fstream f_stream = io.getFileStream();
+            std::stringstream buffer;
+            buffer << f_stream.rdbuf();
+            std::string key = buffer.str();
+            return key;
+        }
+};
+
+
